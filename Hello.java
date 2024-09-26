7f08c7e2b9db74bb0395fe4a63032a09a8a7b472
Chudamani Charitha <nuguricharitha@gmail.com>
2024-09-26 16:56:48 +0530

    I have added the hii,hy and hello file to the existing file.

---
+++ Hello.java
@@ -0,0 +1,8 @@
+//Hello class definition
+class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	}
+}
---
+++ Hy.java
@@ -0,0 +1,8 @@
+//Hy class definition
+class Hy
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hy");
+	}
+}
